4bc0b6e810543bb5c129110c5455ae7398cf3ee1
Paweł Hofman <pawel.hofman@sweepr.com>
2022-01-06 01:53:44 +0100

    Updated control reference in an example

--- paho.mqtt.android.example/src/main/java/paho/mqtt/java/example/PahoExampleActivity.java
+++ paho.mqtt.android.example/src/main/java/paho/mqtt/java/example/PahoExampleActivity.java
@@ -24,6 +24,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
